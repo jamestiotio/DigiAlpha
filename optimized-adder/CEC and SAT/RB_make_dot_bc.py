1f9ea6f27ff7c544d587b43220d8adf0eb4c75bd
Ragul Balaji <ragulbaby@gmail.com>
2020-10-22 15:52:24 +0800

    fail :(

--- optimized-adder/CEC and SAT/RB_make_dot_bc.py
+++ optimized-adder/CEC and SAT/RB_make_dot_bc.py
@@ -2,10 +2,8 @@
 # SUTD 50.002 Computation Structures 2D WEEK!
 # Print BC file stuffs to STDOUT
 
-# BC File Header
-print("BC1.1")
-
 def RippleCarry32BC():
+  print("\n") # Neat
   # Generate XB
   for i in range(32):
     print("B%d := ODD(Bin%d,OP0);" % (i, i)) # invert B if op0
@@ -21,4 +19,42 @@ def RippleCarry32BC():
   for i in range(32):
     print("S%d := ODD(A%d,B%d,C%d);" % (i, i, i, i)) # SUMi  = Ai XOR Bi XOR Ci
 
-RippleCarry32BC()
+def KoggeStone32BC():
+  print("\n") # Neat
+  # Generate Propgate Cells
+
+  print("kek0 := AND(A0,B0);") # kek0 = Ai AND Bi
+  print("L0G0 := OR(AND(L0P0, OP0),kek0);") # gg = (pt AND gl) OR gt
+  for i in range(1,32):
+    print("L0G%d := AND(A%d,B%d);" % (i, i, i)) # Gi = Ai AND Bi
+
+  print("") # Neat
+  for i in range(32):
+    print("L0P%d := ODD(A%d,B%d);" % (i, i, i)) # Pi = Ai XOR Bi
+
+  # Layers
+  for l in range(5):
+    print("\n") # Neat
+    for i in range(2**l):
+      print("L%dG%d := (L%dG%d);" % (l+1, i, l, i)) # PASSTHRU
+    for i in range(2**l,32):
+      print("L%dG%d := OR(AND(L%dP%d, L%dG%d),L%dG%d);" % (l+1, i, l, i, l, i-1, l, i)) # GG
+    for i in range(2**(l+1),32):
+      print("L%dP%d := AND(L%dP%d, L%dP%d);" % (l+1, i, l, i, l, i-1)) # PP
+
+  # Compute Sum
+  print("") # Neat
+  print("KSUM0 := ODD(L0P0, OP0);") # SPECIAL CASE
+  for i in range(1,32):
+    print("KSUM%d := ODD(L0P%d, L5G%d);" % (i, i, i-1)) # XOR for sum
+
+if __name__ == '__main__':
+  print("BC1.1") # BC File Header
+  RippleCarry32BC()
+  KoggeStone32BC()
+
+  # TEST IF MSB IS EQUAL
+  print("") # Neat
+  print("Z := ODD(S31, KSUM31);")
+  print("ASSIGN Z;")
+  
